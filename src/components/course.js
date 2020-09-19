df32400780ca0954bb97828cc4754216c8410133
Mindren Lu <mdlu@mit.edu>
2020-09-19 02:07:58 -0500

    adding classes

--- src/components/course.js
+++ src/components/course.js
@@ -7,8 +7,27 @@ var courses = new Vue({
     methods: {
         addCourse: function() {
             var course = $('#courseNumber').val();
-            courses.courseList.push(course);
+            var courseItem;
+            if (course.toString() in classes) {
+                courseItem = {number: course, name: classes[course.toString()]['n']};
+            } else {
+                courseItem = {number: course, name: "No Class Found"};
+            }
+
+            // TODO trying to not allow duplicate classes but accessing courseList gives weird behavior sigh
+            var courseSet = new Set();
+            for (c in this.courseList) {
+                courseSet.add(c.name); // this breaks sadly I hate Vue
+            }
+            if (!(courseSet.has(course))) {
+                this.courseList.push(courseItem);
+                this.courseList.sort((a, b) => a.number - b.number);
+            }
             $('#courseNumber').val('');
-        }
+        },
+
+        deleteCourse: function(course) {
+            this.courseList.splice(this.courseList.indexOf(course), 1);
+        },
     },
 });
