6e650d96bca7e71ea166bf4facf7c78466e2ae33
Lucy Zhang <zhangly@mit.edu>
2020-09-20 09:06:32 -0400

    cleanup

--- vue-app/src/components/MajorForm.vue
+++ vue-app/src/components/MajorForm.vue
@@ -52,33 +52,21 @@
         console.log(year);
 
         // major: "sample road"
-        var maps = {"4: Architecture": {"Fall 2020": [{name: "2.00"}], "Spring 2021": [{name: "6.0002"}]},
-                    "6-3: Computer Science": {"Fall 2020": [{name: "6.0001"}, {name: "18.02"}, {name: "8.02"}, {name: "CI-H"}],
-                      "Spring 2021": [{name: "6.042"}, {name: "6.009"}, {name: "7.012"}, {name: "HASS"}],
-                      "Fall 2021": [{name: "6.006"}, {name: "6.004"}, {name: "3.091"}, {name: "CI-H"}],
-                      "Spring 2022": [{name: "6.046"}, {name: "6.01"}, {name: "HASS"}, {name: "6.033"}],
-                      "Fall 2022": [{name: "6.031"}, {name: "6.UAT"}, {name: "18.03"}, {name: "HASS"}],
-                      "Fall 2023": [{name: "6.840"}, {name: "6.036"}, {name: "HASS"}, {name: "HASS"}],
-                      "Spring 2023": [{name: "6.215"}, {name: "HASS"}, {name: "6.172"}],
-                      "Spring 2024": [{name: "HASS"}, {name: "7.03"}, {name: "6.047"}]}};
-        console.log("here"+maps[major.value]);
-        this.$emit('update:road', maps[major.value]);
 
-        // TODO this is all broken and I spent too long on this and am crying a lot
-        // let self = this;
-        // $.post("http://localhost:5000/apiCall", {'major': major.value, 'year': year.value}, function(data) {
-        //   console.log(data);
-        //   // var newData = JSON.parse(JSON.stringify(data));
-        //   // console.log(newData);
-        //   // self.$emit('update:road', newData);
+        let self = this;
+        $.post("http://localhost:5000/apiCall", {'major': major.value, 'year': year.value}, function(data) {
+          console.log(data);
+          // var newData = JSON.parse(JSON.stringify(data));
+          // console.log(newData);
+          // self.$emit('update:road', newData);
           
-        //   var data2 = {"Fall 2020": [{name: "6.0001"}], "Spring 2021": [{name: "6.0002"}]};
-        //   self.$emit('update:road', data2);
-        // });
+          var data2 = {"Fall 2020": [{name: "6.0001"}], "Spring 2021": [{name: "6.0002"}]};
+          self.$emit('update:road', data2);
+        });
 
-        // var data = {"Fall 2020": [{name: "6.0001"}], "Spring 2021": [{name: "6.0002"}]};
-        // console.log(data);
-        // this.$emit('update:road', data);
+        var data = {"Fall 2020": [{name: "6.0001"}], "Spring 2021": [{name: "6.0002"}]};
+        console.log(data);
+        this.$emit('update:road', data);
       }
     }
   }
