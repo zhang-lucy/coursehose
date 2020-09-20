04793fc7e1e1a114e1eee1067304bb67a8b84c90
Melinda Sun <mmsun@mit.edu>
2020-09-19 18:35:44 -0700

    added prereq parsing

--- backend/server.py
+++ backend/server.py
@@ -1,26 +1,82 @@
-def get_major_prereqs(major):
-	# idk????? smth to do w fireroad api?????
-	# currently assuming this just returns a list of classes but unsure how to handle weird cases 
-	# (and most things are weird cases)
-	prereqs = {}
-	return prereqs
+import json
+import re
+import ast
 
-def get_minor_prereqs(minor):
-	# ignore minors for now
-	return {}
+all_courses_file = "../data/allCourses.json"
+major_reqs_file = "../data/course_requirements.json"
+
+
+def read_data_files(all_courses_file, major_reqs_file):
+    with open(all_courses_file, 'r') as f:
+        all_courses = json.load(f)
+    with open(major_reqs_file, 'r') as f:
+        major_reqs = json.load(f)
+    return all_courses, major_reqs
+
+
+def parse_condition(condition):
+    if 'req' in condition:
+        return condition['req']
+    reqs = []
+    if condition['connection-type'] == 'all':
+        for subcondition in condition['reqs']:
+            pass
+
+
+def get_major_reqs(major):
+    '''
+	Give requirements for a major.
+	:param major: major code, e.g. 'major18pm'
+	:return: Requirements in an undetermined format?? {(course1, ... ): num_required}
+	'''
+    # idk????? smth to do w fireroad api?????
+    # currently assuming this just returns a list of classes but unsure how to handle weird cases
+    # (and most things are weird cases)
+    reqs = {}
+    req_dict = major_reqs[major]
+    for condition in req_dict:
+        pass
+
+    return reqs
+
+
+def get_minor_reqs(minor):
+    # ignore minors for now
+    return {}
+
 
 def get_prereqs(course):
-	# also sth to do with the fireroad api/our backend??
-	# again will assume this just returns a nice list of prereqs even tho probably fake news
-	prereqs = []
-	return prereqs
+    # also sth to do with the fireroad api/our backend??
+    # again will assume this just returns a nice list of prereqs even tho probably fake news
+    prereq_str = all_courses[course]['pr']
+    if prereq_str in ("None", "Permission of instructor"):
+        return []
+    GIR_courses = {"Calculus I (GIR)": "18.01", "Calculus II (GIR)": "18.02", "Physics I (GIR)": "8.01",
+                   "Physics II (GIR)": "8.02", "Biology (GIR)": "(7.012, 7.013, 7.014, 7.015, or 7.016",
+                   "Chemistry": "(3.091, 5.111, 5.112)"}
+    for GIR in GIR_courses:
+        prereq_str = prereq_str.replace(GIR, GIR_courses[GIR])
+    prereq_str = prereq_str.replace(" or permission of instructor", "")
+    prereq_str = re.sub("[\[\] ]", "", prereq_str)
+    prereq_str = re.sub("and|or", ",", prereq_str)
+    prereq_str = re.sub(",", ",,", prereq_str)
+    prereq_str = "[" + prereq_str + "]"
+    prereq_str = re.sub("([\[\]\(\),])([\w.]+)([\[\]\(\),])",
+                        lambda x: x.group(1) + "'" + x.group(2) + "'" + x.group(3), prereq_str)
+    prereq_str = re.sub(",,", ",", prereq_str)
+    return ast.literal_eval(prereq_str)
+
+
+# return json.loads(prereq_str, parse_float=lambda x:x, parse_int=lambda x:x)
+
 
 def find_offerings(course, start_semester, end_semester):
-	# returns a list of all semester numbers this class will be offered
-	# again vaguely sketchy, this time bc mit can be sketchy abt this
-	# but we will just assume for now that generally actual prereqs are not that sketchy
-	semesters = []
-	return semesters
+    # returns a list of all semester numbers this class will be offered
+    # again vaguely sketchy, this time bc mit can be sketchy abt this
+    # but we will just assume for now that generally actual prereqs are not that sketchy
+    semesters = []
+    return semesters
+
 
 def find_schedule(majors, minors, start_semester, end_semester = start_semester + 7, past_schedule = {}, existing_schedule = {}, cost_function = "classes"):
 	# can prob just assume semesters are numbered as 2*(year - 2000) + (1 if fall) or sth
@@ -39,3 +95,7 @@ def find_schedule(majors, minors, start_semester, end_semester = start_semester 
 		# binary search, maybe refactor later to pass to other fn
 
 		if ()
+
+if __name__ == "__main__":
+    all_courses, major_reqs = read_data_files(all_courses_file, major_reqs_file)
+    print(get_prereqs("9.016"))
